5bd8a289cc531c1ca3d8aa6b523b0065c90d9668
Arjay Orbita <planetorbita@gmail.com>
2021-02-03 20:08:33 -0800

    Add project files.

---
+++ HeadFirstCSharp5-1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeadFirstCSharp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SwordDamage swordDamage = new SwordDamage();
+            Random RNG = new Random();
+            while (true)
+            {
+                Console.Write("0 for standard physical damage. 1 for magic damage. 2 for flaming damage. 3 for both, anything else to quit.");
+                char keyPressed = Console.ReadKey().KeyChar;
+                if (keyPressed != '0' && keyPressed != '1' && keyPressed != '2' && keyPressed != '3')
+                    return;
+                int roll = RNG.Next(1, 7) + RNG.Next(1, 7) + RNG.Next(1, 7);
+                swordDamage.Roll = roll;
+                swordDamage.SetMagic(keyPressed == '1' || keyPressed == '3');
+                swordDamage.SetFlaming(keyPressed == '2' || keyPressed == '3');
+                Console.WriteLine($"\nYou rolled {roll} for {swordDamage.Damage} damage.\n");
+
+
+            }
+
+
+        }
+    }
+}
---
+++ HeadFirstCSharp5-1/SwordDamage.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeadFirstCSharp5
+{
+    class SwordDamage
+    {
+        public const int BASE_DAMAGE = 3;
+        public const int FLAME_DAMAGE = 2;
+
+        public int Roll;
+        public decimal Multiplier = 1M;
+        public int FlamingDamage = 0;
+        public int Damage;
+
+        public void CalculateDamage()
+        {
+            Damage = (int)(Roll * Multiplier) + BASE_DAMAGE + FlamingDamage;
+        }
+
+        public void SetMagic(bool isMagic)
+        {
+            if (isMagic)
+                Multiplier = 1.75M;
+            else
+                Multiplier = 1M;
+            CalculateDamage();
+        }
+
+        public void SetFlaming(bool isFlaming)
+        {
+            CalculateDamage();
+            if (isFlaming)
+            {
+                Damage += FLAME_DAMAGE;
+            }
+        }
+    }
+}
